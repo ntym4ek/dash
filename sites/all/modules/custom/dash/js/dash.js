64a0af5b4b0f2c96e8e2ffdd0d70e1494d8060ad
ntym4ek <mega_comp@mail.ru>
2023-10-04 09:44:09 +0300

    правка ошибки многократных ajax запросов

--- sites/all/modules/custom/dash/js/dash.js
+++ sites/all/modules/custom/dash/js/dash.js
@@ -2,30 +2,31 @@
   Drupal.behaviors.dash = {
     attach: function (context, settings) {
 
-      $(".ajax-load").each(function() {
-        var content = $(this).find(".row");
-        if (!content.length) {
-          var wrapper = $(this);
-          var module = $(this).closest(".charts").data("module");
-          var func = $(this).data("func");
-          var periodFrom = $(this).closest(".charts").data("period-from");
-          var periodTo = $(this).closest(".charts").data("period-to");
-          var delay = $(this).data("delay");
-          if (module && func) {
-            setTimeout(function() {
-              $.ajax({
-                url: "/ajax-load?module=" + module + "&func=" + func + "&period_from=" + periodFrom + "&period_to=" + periodTo,
-                success: function (data) {
-                  // wrapper.html(data);
-                  var newNodes = wrapper.html(data);
-                  Drupal.attachBehaviors(newNodes);
-                }
-              });
-            }, delay);
+      $(".charts").once(function(){
+        $(".ajax-load").each(function() {
+          var content = $(this).find(".row");
+          if (!content.length) {
+            var wrapper = $(this);
+            var module = $(this).closest(".charts").data("module");
+            var func = $(this).data("func");
+            var periodFrom = $(this).closest(".charts").data("period-from");
+            var periodTo = $(this).closest(".charts").data("period-to");
+            var delay = $(this).data("delay");
+            if (module && func) {
+              setTimeout(function() {
+                $.ajax({
+                  url: "/ajax-load?module=" + module + "&func=" + func + "&period_from=" + periodFrom + "&period_to=" + periodTo,
+                  success: function (data) {
+                    // wrapper.html(data);
+                    var newNodes = wrapper.html(data);
+                    Drupal.attachBehaviors(newNodes);
+                  }
+                });
+              }, delay);
+            }
           }
-        }
+        });
       });
-
     }
   };
 })(jQuery);
